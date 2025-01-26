d09ec921aacc9121f1797f7f13d481e6d37e42de
Esther Wangari <essywangari06@gmail.com>
2025-01-26 17:51:22 +0300

    Validating the value of day, month and year

--- scripts/script.js
+++ scripts/script.js
@@ -17,3 +17,16 @@ let DD = parseInt(document.getElementById("day").value);
 let MM = parseInt(document.getElementById("month").value);
 let YY = parseInt(document.getElementById("year").value);
 let gender = document.querySelector('input[name="gender"]:checked').value;
+
+if (DD <= 0 || DD > 31) {
+  alert("Invalid day");
+  return;
+}
+if (MM <= 0 || MM > 12) {
+  alert("Invalid month");
+  return;
+}
+if (YY<= 1900 || YY > 2025) {
+  alert("Invalid century");
+  return;
+}

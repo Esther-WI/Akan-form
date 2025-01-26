8aecf4edd2337fd889513e1d9bcb856eabafc42d
Esther Wangari <essywangari06@gmail.com>
2025-01-26 17:49:21 +0300

    Preventing the default behaviour

--- scripts/script.js
+++ scripts/script.js
@@ -8,3 +8,12 @@ const maleNames = [
   "Kwame",
 ];
 const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+function calculateAkanName(event) {
+  event.preventDefault();
+
+
+let DD = parseInt(document.getElementById("day").value);
+let MM = parseInt(document.getElementById("month").value);
+let YY = parseInt(document.getElementById("year").value);
+let gender = document.querySelector('input[name="gender"]:checked').value;
